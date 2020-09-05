1f0ce9fe82c569e34b724321bcc23ad34cd7e4c1
Kunwar Abhinav Aditya <kunwar.aditya@scout24.com>
2020-09-05 13:31:10 +0200

    Add test

---
+++ src/test/java/com/amigoscode/demo/student/StudentControllerTest.java
@@ -0,0 +1,36 @@
+package com.amigoscode.demo.student;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentControllerTest {
+
+  @Before
+  public void setUp() {
+  }
+
+  @After
+  public void tearDown() {
+  }
+
+  @Test
+  public void getAllStudents() {
+  }
+
+  @Test
+  public void getAllCoursesForStudent() {
+  }
+
+  @Test
+  public void addNewStudent() {
+  }
+
+  @Test
+  public void updateStudent() {
+  }
+
+  @Test
+  public void deleteStudent() {
+  }
+}
